2bb6bbf2b33af37b466ad68b583074b1d09f85b6
tranlamdat <dattlgbc200045@fpt.edu.vn>
2022-10-19 09:04:38 +0700

    index

--- Index.php
+++ Index.php
@@ -92,7 +92,7 @@ include_once("connection.php");
     <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
       <!-- Left -->
       <div class="me-5 d-none d-lg-block">
-        <span>Get connected with us on social networks:</span>
+        <span>Get connected with us on social network:</span>
       </div>
       <!-- Left -->
 
