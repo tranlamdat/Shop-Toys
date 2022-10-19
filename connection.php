cab213d0780ef0c463fb08f8515de69549c82e2d
tranlamdat <dattlgbc200045@fpt.edu.vn>
2022-10-19 08:22:40 +0700

    index

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
-
-    $Connect = pg_connect("postgres://bkywifrawnzrpp:cef1a35531f257bf3f30b8cda0051bc7f9a9702d0f1860f904513cde7a35c8fc@ec2-3-219-19-205.compute-1.amazonaws.com:5432/ddqh3pdv9gqdeh");
+    $Connect = pg_connect("postgres://noryqjwjksupcw:f48c0f1d87a7ea3c09bf586a9aad531b044801c6f9df7adb1f61f626e5789441@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d8gv0t1fd4kg42");
     if (!$Connect) {
         die("Connection failed");
     }
