bc31c3e22d36b7b74a9f8b9957ddb7c67cf18a40
tranlamdat <dattlgbc200045@fpt.edu.vn>
2022-10-21 08:50:15 +0700

    user

--- Register.php
+++ Register.php
@@ -60,12 +60,6 @@ if (isset($_POST['btnRegister'])) {
     <h2>Member Registration</h2>
     <form id="form1" name="form1" method="post" action="" class="form-horizontal" role="form">
     <div class="form-group">
-
-<label for="txtTen" class="col-sm-2 control-label">UserID(*): </label>
-<div class="col-sm-10">
-    <input type="text" name="txtUserID" id="txtUserID" class="form-control" placeholder="UserID" value="" />
-</div>
-</div>
         <div class="form-group">
 
             <label for="txtTen" class="col-sm-2 control-label">Username(*): </label>
